8ab047161e09fdd168ef6a7528b31a3632dfbbc1
abdurashidovabdurashidov630-svg <abdurashidovabdurashidov630@gmail.com>
2026-04-16 20:06:37 +0500

    Function

---
+++ homework10/script.js
@@ -0,0 +1,168 @@
+// function mashq1() {
+//   for (let i = 5; i <= 15; i++) {
+//     console.log(i);
+//   }
+// }
+// mashq1();
+
+// function mashq2() {
+//   let yigindi = 0;
+//   for (let i = 1; i <= 200; i++) {
+//     yigindi += i;
+//   }
+//   console.log(yigindi);
+// }
+// mashq2();
+
+// function mashq3() {
+//   for (let i = 1; i <= 60; i++) {
+//     if (i % 2 === 0) console.log(i);
+//   }
+// }
+// mashq3();
+
+// function mashq4() {
+//   for (let i = 1; i <= 60; i++) {
+//     if (i % 2 !== 0) console.log(i);
+//   }
+// }
+// mashq4();
+
+// function mashq5() {
+//   let n = +prompt("N:");
+//   for (let i = 1; i <= n; i++) {
+//     console.log(i ** 3);
+//   }
+// }
+// mashq5();
+
+// function mashq6() {
+//   let n = +prompt("N:");
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 4 === 0) console.log(i);
+//   }
+// }
+// mashq6();
+
+// function mashq7() {
+//   let n = +prompt("N:");
+//   let yigindi = 0;
+//   for (let i = 1; i <= n; i++) {
+//     yigindi += i;
+//   }
+//   console.log(yigindi);
+// }
+// mashq7();
+
+// function mashq8() {
+//   for (let i = 1; i <= 150; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) console.log(i);
+//   }
+// }
+// mashq8();
+
+// function mashq9() {
+//   for (let i = 20; i >= 1; i--) {
+//     console.log(i);
+//   }
+// }
+// mashq9();
+
+// function mashq10() {
+//   let n = +prompt("N:");
+//   let yigindi = 0;
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 2 === 0) yigindi += i;
+//   }
+//   console.log(yigindi);
+// }
+// mashq10();
+
+// function mashq11() {
+//   let n = +prompt("N:");
+//   let count = 0;
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 2 !== 0) count++;
+//   }
+//   console.log(count);
+// }
+// mashq11();
+
+// function mashq12() {
+//   let n = +prompt("N:");
+//   let min = 1;
+//   for (let i = 1; i <= n; i++) {
+//     if (i < min) min = i;
+//   }
+//   console.log(min);
+// }
+// mashq12();
+
+// function mashq13() {
+//   let count = 0;
+//   for (let i = 1; i <= 30; i++) {
+//     if (i % 3 === 0) count++;
+//   }
+//   console.log(count);
+// }
+// mashq13();
+
+// function mashq14() {
+//   let n = +prompt("N:");
+//   for (let i = 1; i <= n; i++) {
+//     if (i < 20) console.log(i);
+//   }
+// }
+// mashq14();
+
+// function mashq15() {
+//   let n = +prompt("N:");
+//   for (let i = 1; i <= 10; i++) {
+//     console.log(n + i);
+//   }
+// }
+// mashq15();
+
+// function mashq16() {
+//   for (let i = 1; i <= 100; i++) {
+//     if (i % 9 === 0) console.log(i);
+//   }
+// }
+// mashq16();
+
+// function mashq17() {
+//   let n = +prompt("N:");
+//   let max = 0;
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 2 !== 0) max = i;
+//   }
+//   console.log(max);
+// }
+// mashq17();
+
+// function mashq18() {
+//   for (let i = 1; i <= 40; i++) {
+//     if (i % 6 !== 0) console.log(i);
+//   }
+// }
+// mashq18();
+
+// function mashq19() {
+//   let n = +prompt("N:");
+//   let count = 0;
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 2 === 0 && i % 3 === 0) count++;
+//   }
+//   console.log(count);
+// }
+// mashq19();
+
+// function mashq20() {
+//   let n = +prompt("N:");
+//   let yigindi = 0;
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 5 === 0) yigindi += i;
+//   }
+//   console.log(yigindi);
+// }
+// mashq20();
