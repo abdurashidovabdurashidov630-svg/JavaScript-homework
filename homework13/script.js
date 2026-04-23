6ab6a8e7278143a3c047886c10748fa0c1d1c2fc
abdurashidovabdurashidov630-svg <abdurashidovabdurashidov630@gmail.com>
2026-04-23 20:01:08 +0500

    Object

---
+++ homework13/script.js
@@ -0,0 +1,129 @@
+// let Yahyobek = {
+//   ism: "Yahyobek",
+//   familiya: "Xakimjanov",
+//   yosh: 12,
+//   maktabgaBoradimi: true,
+//   manzil: "Namangan",
+//   telRaqam: "Yo'q",
+// };
+
+// console.log(Yahyobek.ism, Yahyobek.familiya, Yahyobek.yosh, Yahyobek.maktabgaBoradimi, Yahyobek.manzil, Yahyobek.telRaqam);
+
+// console.log(Yahyobek["ism"], Yahyobek["familiya"], Yahyobek["yosh"], Yahyobek["maktabgaBoradimi"], Yahyobek["manzil"], Yahyobek["telRaqam"]);
+
+// let Behruzbek = {
+//   ism: "Behruzbek",
+//   familiya: "Abduraqibov",
+//   yosh: 13,
+//   maktabgaBoradimi: false,
+//   manzil: "Toshkent",
+//   telRaqam: "+998 93 111 56 16",
+// };
+
+// console.log(Behruzbek.ism, Behruzbek.familiya, Behruzbek.yosh, Behruzbek.maktabgaBoradimi, Behruzbek.manzil, Behruzbek.telRaqam);
+
+// console.log(Behruzbek["ism"], Behruzbek["familiya"], Behruzbek["yosh"], Behruzbek["maktabgaBoradimi"], Behruzbek["manzil"], Behruzbek["telRaqam"]);
+
+// let Kamron = {
+//   ism: "Kamron",
+//   familiya: "Ne'matjanov",
+//   yosh: 14,
+//   maktabgaBoradimi: false,
+//   manzil: "Andijon",
+//   telRaqam: "+998 93 112 57 17",
+// };
+
+// console.log(Kamron.ism, Kamron.familiya, Kamron.yosh, Kamron.maktabgaBoradimi, Kamron.manzil, Kamron.telRaqam);
+
+// console.log(Kamron["ism"], Kamron["familiya"], Kamron["yosh"], Kamron["maktabgaBoradimi"], Kamron["manzil"], Kamron["telRaqam"]);
+
+// let Suhrob = {
+//   ism: "Suhrob",
+//   familiya: "Ne'matjanov",
+//   yosh: 15,
+//   maktabgaBoradimi: true,
+//   manzil: "Farg'ona",
+//   telRaqam: "+998 93 113 58 18",
+// };
+
+// console.log(Suhrob.ism, Suhrob.familiya, Suhrob.yosh, Suhrob.maktabgaBoradimi, Suhrob.manzil, Suhrob.telRaqam);
+
+// console.log(Suhrob["ism"], Suhrob["familiya"], Suhrob["yosh"], Suhrob["maktabgaBoradimi"], Suhrob["manzil"], Suhrob["telRaqam"]);
+
+// let Shohjahon = {
+//   ism: "Shohjahon",
+//   familiya: "Shermirzayev",
+//   yosh: 16,
+//   maktabgaBoradimi: false,
+//   manzil: "Samarqand",
+//   telRaqam: "+998 94 114 59 19",
+// };
+
+// console.log(Shohjahon.ism, Shohjahon.familiya, Shohjahon.yosh, Shohjahon.maktabgaBoradimi, Shohjahon.manzil, Shohjahon.telRaqam);
+
+// console.log(Shohjahon["ism"], Shohjahon["familiya"], Shohjahon["yosh"], Shohjahon["maktabgaBoradimi"], Shohjahon["manzil"], Shohjahon["telRaqam"]);
+
+// let Behruzbek2 = {
+//   ism: "Behruzbek2",
+//   familiya: "Bohodirov",
+//   yosh: 17,
+//   maktabgaBoradimi: true,
+//   manzil: "Qashqadaryo",
+//   telRaqam: "+998 93 121 16 56",
+// };
+
+// console.log(Behruzbek2.ism, Behruzbek2.familiya, Behruzbek2.yosh, Behruzbek2.maktabgaBoradimi, Behruzbek2.manzil, Behruzbek2.telRaqam);
+
+// console.log(Behruzbek2["ism"], Behruzbek2["familiya"], Behruzbek2["yosh"], Behruzbek2["maktabgaBoradimi"], Behruzbek2["manzil"], Behruzbek2["telRaqam"]);
+
+// let Mirjalol = {
+//   ism: "Mirjalol",
+//   familiya: "Mahmudjanov",
+//   yosh: 18,
+//   maktabgaBoradimi: false,
+//   manzil: "Sirdaryo",
+//   telRaqam: "+998 94 213 14 24",
+// };
+
+// console.log(Mirjalol.ism, Mirjalol.familiya, Mirjalol.yosh, Mirjalol.maktabgaBoradimi, Mirjalol.manzil, Mirjalol.telRaqam);
+
+// console.log(Mirjalol["ism"], Mirjalol["familiya"], Mirjalol["yosh"], Mirjalol["maktabgaBoradimi"], Mirjalol["manzil"], Mirjalol["telRaqam"]);
+
+// let Abdulvohid = {
+//   ism: "Abdulvohid",
+//   familiya: "Jo'rahanov",
+//   yosh: 19,
+//   maktabgaBoradimi: true,
+//   manzil: "Xorazm",
+//   telRaqam: "+998 93 141 36 66",
+// };
+
+// console.log(Abdulvohid.ism, Abdulvohid.familiya, Abdulvohid.yosh, Abdulvohid.maktabgaBoradimi, Abdulvohid.manzil, Abdulvohid.telRaqam);
+
+// console.log(Abdulvohid["ism"], Abdulvohid["familiya"], Abdulvohid["yosh"], Abdulvohid["maktabgaBoradimi"], Abdulvohid["manzil"], Abdulvohid["telRaqam"]);
+
+// let Begzod = {
+//   ism: "Begzod",
+//   familiya: "Abdug'aniyev",
+//   yosh: 20,
+//   maktabgaBoradimi: false,
+//   manzil: "Buxoro",
+//   telRaqam: "+998 93 222 22 32",
+// };
+
+// console.log(Begzod.ism, Begzod.familiya, Begzod.yosh, Begzod.maktabgaBoradimi, Begzod.manzil, Begzod.telRaqam);
+
+// console.log(Begzod["ism"], Begzod["familiya"], Begzod["yosh"], Begzod["maktabgaBoradimi"], Begzod["manzil"], Begzod["telRaqam"]);
+
+// let Shukurullo = {
+//   ism: "Shukurullo",
+//   familiya: "Tursunboyev",
+//   yosh: 21,
+//   maktabgaBoradimi: true,
+//   manzil: "Navoiy",
+//   telRaqam: "+998 93 131 12 02",
+// };
+
+// console.log(Shukurullo.ism, Shukurullo.familiya, Shukurullo.yosh, Shukurullo.maktabgaBoradimi, Shukurullo.manzil, Shukurullo.telRaqam);
+
+// console.log(Shukurullo["ism"], Shukurullo["familiya"], Shukurullo["yosh"], Shukurullo["maktabgaBoradimi"], Shukurullo["manzil"], Shukurullo["telRaqam"]);
