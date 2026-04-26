34dd5eb455d92966edf4652ce536063550f9dd2b
abdurashidovabdurashidov630-svg <abdurashidovabdurashidov630@gmail.com>
2026-04-26 20:03:19 +0500

    Object

---
+++ homework14/script.js
@@ -0,0 +1,219 @@
+// let Shukurullo = {
+//   ism: "Shukurullo",
+//   familiya: "Tursunboyev",
+//   yosh: 21,
+//   maktabgaBoradimi: true,
+//   manzil: "Navoiy",
+//   telRaqam: "+998 93 131 12 02",
+// };
+// let Behruzbek = Object.assign({},Shukurullo)
+// Shukurullo.ism = "Behruzbek"
+// Shukurullo.familiya = "Bohodirov"
+// Shukurullo.yosh = 13
+// Shukurullo.manzil = "Namangan"
+
+// let Abdulvohid = structuredClone(Shukurullo)
+// Shukurullo.ism = "Abdulvohid"
+// Shukurullo.familiya = "Jo'rahanov"
+// Shukurullo.yosh = 15
+// Shukurullo.manzil = "Namangan"
+// console.log(Shukurullo);
+// console.log(Abdulvohid);
+
+// let Begzod = {
+//   ism: "Begzod",
+//   familiya: "Abdug'aniyev",
+//   yosh: 20,
+//   maktabgaBoradimi: false,
+//   manzil: "Buxoro",
+//   telRaqam: "+998 93 222 22 32",
+// };
+// let Behruzbek2 = Object.assign({},Begzod)
+// Begzod.ism = "Behruzbek"
+// Begzod.familiya = "Abduraqibov"
+// Begzod.yosh = 16
+// Begzod.manzil = "Navoiy"
+
+// let Kamron = structuredClone(Begzod)
+// Begzod.ism = "Kamron"
+// Begzod.familiya = "Ne'matjanov"
+// Begzod.yosh = 18
+// Begzod.manzil = "Farg'ona"
+// console.log(Begzod);
+// console.log(Kamron);
+
+// let Ali = {
+//   ism: "Ali",
+//   familiya: "Valiyev",
+//   yosh: 31,
+//   maktabgaBoradimi: false,
+//   manzil: "Navoiy",
+//   telRaqam: "+998 93 131 12 02",
+// };
+// let Vali = Object.assign({},Ali)
+// Ali.ism = "Vali"
+// Ali.familiya = "Aliyev"
+// Ali.yosh = 43
+// Ali.manzil = "Namangan"
+
+// let Eshmat = structuredClone(Ali)
+// Ali.ism = "Eshmat"
+// Ali.familiya = "Toshmatov"
+// Ali.yosh = 51
+// Ali.manzil = "Qashqadaryo"
+// console.log(Eshmat);
+// console.log(Ali);
+
+// let Yahyo = {
+//   ism: "Yahyo",
+//   familiya: "Xakimjanov",
+//   yosh: 20,
+//   maktabgaBoradimi: false,
+//   manzil: "Buxoro",
+//   telRaqam: "+998 93 222 22 32",
+// };
+// let Mustafo = Object.assign({},Yahyo)
+// Yahyo.ism = "Mustafo"
+// Yahyo.familiya = "Abdurashidov"
+// Yahyo.yosh = 16
+// Yahyo.manzil = "Navoiy"
+
+// let Mustafo2 = structuredClone(Yahyo)
+// Yahyo.ism = "Mustafo"
+// Yahyo.familiya = "Abdutolipov"
+// Yahyo.yosh = 18
+// Yahyo.manzil = "Farg'ona"
+// console.log(Yahyo);
+// console.log(Mustafo2);
+
+// let Jamshid = {
+//   ism: "Jamshid",
+//   familiya: "Karimov",
+//   yosh: 25,
+//   maktabgaBoradimi: false,
+//   manzil: "Samarqand",
+//   telRaqam: "+998 90 111 11 11",
+// };
+// let Dilshod = Object.assign({}, Jamshid)
+// Jamshid.ism = "Dilshod"
+// Jamshid.familiya = "Rasulov"
+// Jamshid.yosh = 19
+// Jamshid.manzil = "Andijon"
+
+// let Sardor = structuredClone(Jamshid)
+// Jamshid.ism = "Sardor"
+// Jamshid.familiya = "Xolmatov"
+// Jamshid.yosh = 22
+// Jamshid.manzil = "Toshkent"
+// console.log(Jamshid);
+// console.log(Sardor);
+
+// let Nodir = {
+//   ism: "Nodir",
+//   familiya: "Sodiqov",
+//   yosh: 28,
+//   maktabgaBoradimi: false,
+//   manzil: "Xorazm",
+//   telRaqam: "+998 91 222 22 22",
+// };
+// let Sherzod = Object.assign({}, Nodir)
+// Nodir.ism = "Sherzod"
+// Nodir.familiya = "Toshpulatov"
+// Nodir.yosh = 24
+// Nodir.manzil = "Buxoro"
+
+// let Azamat = structuredClone(Nodir)
+// Nodir.ism = "Azamat"
+// Nodir.familiya = "Qodirov"
+// Nodir.yosh = 30
+// Nodir.manzil = "Namangan"
+// console.log(Nodir);
+// console.log(Azamat);
+
+// let Islom = {
+//   ism: "Islom",
+//   familiya: "Rahimov",
+//   yosh: 35,
+//   maktabgaBoradimi: false,
+//   manzil: "Toshkent",
+//   telRaqam: "+998 93 333 33 33",
+// };
+// let Farhod = Object.assign({}, Islom)
+// Islom.ism = "Farhod"
+// Islom.familiya = "Yusupov"
+// Islom.yosh = 27
+// Islom.manzil = "Farg'ona"
+
+// let Javohir = structuredClone(Islom)
+// Islom.ism = "Javohir"
+// Islom.familiya = "Bozorov"
+// Islom.yosh = 29
+// Islom.manzil = "Sirdaryo"
+// console.log(Islom);
+// console.log(Javohir);
+
+// let Rustam = {
+//   ism: "Rustam",
+//   familiya: "Ergashev",
+//   yosh: 40,
+//   maktabgaBoradimi: false,
+//   manzil: "Qashqadaryo",
+//   telRaqam: "+998 94 444 44 44",
+// };
+// let Otabek = Object.assign({}, Rustam)
+// Rustam.ism = "Otabek"
+// Rustam.familiya = "Xudoyberdiyev"
+// Rustam.yosh = 33
+// Rustam.manzil = "Surxondaryo"
+
+// let Shoxrux = structuredClone(Rustam)
+// Rustam.ism = "Shoxrux"
+// Rustam.familiya = "Abdullayev"
+// Rustam.yosh = 26
+// Rustam.manzil = "Jizzax"
+// console.log(Rustam);
+// console.log(Shoxrux);
+
+// let Baxtiyor = {
+//   ism: "Baxtiyor",
+//   familiya: "Nasriddinov",
+//   yosh: 23,
+//   maktabgaBoradimi: false,
+//   manzil: "Andijon",
+//   telRaqam: "+998 95 555 55 55",
+// };
+// let Xasan = Object.assign({}, Baxtiyor)
+// Baxtiyor.ism = "Xasan"
+// Baxtiyor.familiya = "Aliyev"
+// Baxtiyor.yosh = 21
+// Baxtiyor.manzil = "Namangan"
+
+// let Husan = structuredClone(Baxtiyor)
+// Baxtiyor.ism = "Husan"
+// Baxtiyor.familiya = "Valiyev"
+// Baxtiyor.yosh = 22
+// Baxtiyor.manzil = "Farg'ona"
+// console.log(Baxtiyor);
+// console.log(Husan);
+
+// let Diyor = {
+//   ism: "Diyor",
+//   familiya: "Mamatov",
+//   yosh: 18,
+//   maktabgaBoradimi: true,
+//   manzil: "Sirdaryo",
+//   telRaqam: "+998 97 666 66 66",
+// };
+// let Zafar = Object.assign({}, Diyor)
+// Diyor.ism = "Zafar"
+// Diyor.familiya = "Tursunov"
+// Diyor.yosh = 20
+// Diyor.manzil = "Toshkent"
+
+// let Bekzod = structuredClone(Diyor)
+// Diyor.ism = "Bekzod"
+// Diyor.familiya = "Qobilov"
+// Diyor.yosh = 19
+// Diyor.manzil = "Samarqand"
+// console.log(Diyor);
+// console.log(Bekzod);
